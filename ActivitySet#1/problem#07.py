10f83036df5f0f9f25faadc93c3077ec1979fd51
Vivekanand Shridhar Deshinge <97461910+Maharshivivekanand@users.noreply.github.com>
2022-05-16 18:53:23 +0000

    p07

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,4 @@
-# Strings
-
 text = "X-DSPAM-Confidence:    0.8475"
+x=text.find('0.8')
+y=float(text[x:])
+print(y)
--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,22 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count=0
+total=0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        
+        continue
+    if line.startswith("X-DSPAM-Confidence:"):
+        
+		m=line.find('0')
+   	 	maggy=float(line[m:])
+    total=total+maggy
+    count=count+1
+                       
+   #print(line)
+   #print(maggy)
+   #print(total)
+   #print(count)
+    
+print('Average spam confidence:',total/count)
