33664746dd0915159e2d10fa925513f409fa1d09
Vivekanand Shridhar Deshinge <97461910+Maharshivivekanand@users.noreply.github.com>
2022-06-03 15:57:32 +0000

    ac 2p2 done

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,15 +1,21 @@
 
 def add(a, b):
-    pass  # ...
-
+    sum=a+b
+    return sum
 
 def output(a, b, sum):
+    print(f"the sum of {a} and {b} is {sum}")
     pass 
-
+def input_two_numbers():
+    a=int(input())
+    b=int(input())
+    return a,b
 
 def main():
     a, b = input_two_numbers()
-    sum = add(a, b)
+  =
+    sum = add(a,b)
+  
 
     output(a, b, sum)
 
