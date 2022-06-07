2d0914b039bb87197de7fd4e575b4c2b69fab427
Vivekanand Shridhar Deshinge <97461910+Maharshivivekanand@users.noreply.github.com>
2022-06-07 10:22:00 +0000

    p5 done

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -1,26 +1,28 @@
-
-
 def get_cs():
-    """get string input"""
-
-
+    return input('enter a string')
 def cs_to_dict(cs):
-    """convert connect string  to a dictionary"""
-
-
+    maharshi = {}
+    m=cs.split(";")
+    print(m)
+    for x in m:
+        maggy=x.split("=")
+        print(maggy)
+        maharshi[maggy[0]]=maggy[1]
+    return maharshi
 def dict_to_cs(d):
-    """convert a dictionary to connect string"""
+    maggy=""
+    sum=0
+    for x in d:
+        maggy+=x+"="+d[x]+";"
 
+        sum+=1
 
+    return maggy
 def main():
     cs = get_cs()
-
     d = cs_to_dict(cs) # convert connect string to a dictionary
     print(d)
-
     cs = dict_to_cs(d)
     print(cs)
-
-
 if __name__ == '__main__':
     main()
