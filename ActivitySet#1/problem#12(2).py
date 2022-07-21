cbe1c8f7cb83238b2b3cb8e5cacbdcf12a2c3ced
GANESH2903 <97089641+GANESH2903@users.noreply.github.com>
2022-07-21 18:22:10 +0000

    p16

--- ActivitySet#1/problem#12(2).py
+++ ActivitySet#1/problem#12(2).py
@@ -1,13 +1,11 @@
 
-import socket
+from socket import *
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock = socket(AF_INET, SOCK_STREAM)
 HOST = "www.data.pr4e.org"
-PORT = 80
-mysock.connect((HOST, PORT))
+
 cmd = "GET http://data.pr4e.org/intro-short.txt HTTP/1.0\n\n".encode()
-#mysock.connect(('www.py4inf.com', 80))
-#cmd = 'GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n'.encode()
+
 mysock.send(cmd)
 
 while True:
--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -4,11 +4,6 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl, sys
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-
 url = input('Enter url: ')
 count = input('Enter count: ')
 position = input('Enter position: ')
